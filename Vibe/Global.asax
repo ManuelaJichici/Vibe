e3763e43c48b2571a2f38d04c17e588fabd726cd
ManuelaJichici <buhuhu_mela@yahoo.com>
2016-06-23 03:03:13 +0300

    vibe

---
+++ Vibe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="Vibe.Global_asax" Language="vb" %>
---
+++ Vibe/scripts/Angular.js
@@ -0,0 +1,6 @@
+﻿var app = angular.module('ang', []);
+
+app.controller(HeaderControler, function ($scope) {
+
+});
+    
